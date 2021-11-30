7b6570c74f454cdf9c740a8907e84b785b3d05ac
Adrian Gielniewski <adrian.gielniewski@silabs.com>
2021-11-30 13:48:49 +0100

    Add foo

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 /**
+ * Foo
+ * @param a A
+ * @param b B
+ * @return a * 2 - b
+ */
+int foo(int a, int b)
+{
+   return a * 2 - b;
+}
+
+/**
  * This is main function
  * @return
  */
 int main() {
     printf("Hello, World!\n");
-    return 0;
+    int f = foo(1, 2);
+    return f;
 }
